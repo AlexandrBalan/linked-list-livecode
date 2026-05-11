e84f16e69625d0a079ba7fd41e94328a33669add
AlexandrBalan <85966686+AlexandrBalan@users.noreply.github.com>
2026-05-11 10:44:01 -0700

    livecode during class, created myNode and anotherNode, also linked them together and point to each other.

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,19 @@
+import java.util.LinkedList;
+
 public class Practice {
     public static void main(String[] args) {
-        
+        ListNode myNode = new ListNode();
+
+        myNode.data = 'x';
+        // System.out.println(myNode.data);
+
+        ListNode anotherNode = new ListNode();
+
+        anotherNode.data = 't';
+
+        myNode.next = anotherNode;
+
+        // System.out.println(myNode.data);
+        // System.out.println(anotherNode.data);
     }
 }
